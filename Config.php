ee5f8c82b8bcd6bc5ccf64b47ae0f8d6b3a0a3fa
Amaranth01 <vanessa.nectoux@gmail.com>
2022-06-27 18:06:22 +0200

    prepare base of project

--- Config.php
+++ Config.php
@@ -1,6 +1,14 @@
 <?php
 
+namespace app;
+
 class Config
 {
+    // Database configuration
+    public const DB_CHARSET = 'utf8';
+    public const DB_NAME = 'jeux';
+    public const DB_HOST = 'localhost';
+    public const DB_USERNAME = 'root';
+    public const DB_PASSWORD = '';
 
 }
